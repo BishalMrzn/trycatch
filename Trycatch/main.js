c1c7da0ef1dcb4b36f5e03cbcfc2f31148679fb4
BishalMrzn <112357355+BishalMrzn@users.noreply.github.com>
2024-07-26 20:43:11 +0545

    Add files via upload

---
+++ Trycatch/main.js
@@ -0,0 +1,34 @@
+setTimeout(()=>{
+
+    console.log("Hacking WI=FI")
+},1000)
+try{
+
+    console.log(Bishal)
+}
+catch(error){
+
+    console.log("hello")
+}
+
+setTimeout(()=>{
+
+    console.log("Fetching User name")
+},2000)
+setTimeout(()=>{
+
+    console.log("WI=FI found")
+},3000)
+setTimeout(()=>{
+
+    console.log("Users Found")
+},4000)
+try{
+    setTimeout(() => {
+        console.log(Notpossible)
+    }, 200);
+
+}
+catch(error){
+
+}
